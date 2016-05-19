5f5b67e120f2e36df81bdd449d082102a69c157a
William MacDonald <wmsmacdonald@gmail.com>
2016-05-19 01:21:22 -0500

    candidates adding

--- public/javascripts/peer.js
+++ public/javascripts/peer.js
@@ -1,5 +1,5 @@
 "use strict";
-
+var peerConnections;
 (function() {
   const WS_HOST = 'ws://127.0.0.1:3131';
   let signalingServer = new WebSocket(WS_HOST);
@@ -16,42 +16,53 @@
     });
 
   let peerConnectionIdSeq = 0;
-  let peerConnections = range(5, function() {
+  peerConnections = range(5, function() {
     return createOfferConnection(signalingServerConnectPromise, peerConnectionIdSeq++);
   });
 
   function createOfferConnection(signalingServerConnectPromise, id) {
     console.log('creating offer connection');
     let peerConnection = new RTCPeerConnection({'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]});
+    var channel = peerConnection.createDataChannel('test');
     let offerPromise = peerConnection.createOffer();
 
     Promise.all([offerPromise, signalingServerConnectPromise])
       .then(function (values) {
-        values[1].send(JSON.stringify({
+        console.log('got local description');
+        wsSendObject(values[1], {
           offer: {
             description: values[0]
           },
-          localConnectionId: id
-        }));
+          localPeerConnectionId: id
+        });
+        peerConnection.onicecandidate = function(event) {
+          if (event.candidate) {
+            console.log('got local candidate');
+            wsSendObject(values[1], {
+              candidate: event.candidate,
+              localPeerConnectionId: id
+            });
+          }
+        };
       });
 
     offerPromise
       .then(function(description) {
         return peerConnection.setLocalDescription(description);
+      })
+      .then(function() {
+        console.log('offer local description set');
       });
 
     signalingServerConnectPromise
       .then(function(signalingServer) {
-        peerConnection.onIceCandidate = function(candidate) {
-          signalingServer.send(JSON.stringify({
 
-          }))
-        }
       });
 
     return {
       id: id,
-      peerConnection: peerConnection
+      peerConnection: peerConnection,
+      channel: channel
     };
   }
 
@@ -66,16 +77,32 @@
 
     if (message.offer && message.offer.description) {
       // connection and answer need to be created;
-      peerConnections.push(createAnswerConnection(message.offer.description, message.remoteId, peerConnectionIdSeq++));
+      var answerConnection = createAnswerConnection(message.offer.description, message.remoteId, peerConnectionIdSeq++);
+      peerConnections.push(answerConnection);
+      while (message.offer.candidates.length > 0) {
+        answerConnection.peerConnection.addIceCandidate(message.offer.candidates.shift());
+        console.log('added remote candidate');
+      }
     }
 
-    else if (message.answer && message.answer.description) {
+    else if (message.peerConnection && message.message.answer) {
       console.log('got remote answer');
       let peerConnection = peerConnections.find(function(peerConnection) {
-        return peerConnection.id === message.localConnectionId;
+        return peerConnection.id === message.localPeerConnectionId;
       });
 
-      peerConnection.peerConnection.setRemoteDescription(new RTCSessionDescription(message.answer.description));
+      peerConnection.peerConnection.setRemoteDescription(new RTCSessionDescription(message.message.answer.description));
+    }
+    else if (message.peerConnection && message.message.candidate) {
+      let peerConnection = peerConnections.find(function(peerConnection) {
+        return peerConnection.id === message.localPeerConnectionId;
+      });
+
+      peerConnection.peerConnection.addIceCandidate(message.message.candidate);
+      console.log('added remote candidate');
+    }
+    else {
+      console.log('unrecognized message: ' + JSON.stringify(message));
     }
 
   }
@@ -90,20 +117,34 @@
 
     answerPromise
       .then(function(description) {
-        peerConnection.setLocalDescription(description);
+        return peerConnection.setLocalDescription(description);
+      })
+      .then(function() {
+        console.log('set answer local description');
       });
 
     Promise.all([answerPromise, signalingServerConnectPromise])
       .then(function(values) {
         console.log('answer created');
-        values[1].send(JSON.stringify({
+        wsSendObject(values[1],{
           answer: {
             description: values[0]
           },
           targetId: remoteId,
-          localConnectionId: id
-        }));
+          localPeerConnectionId: id
+        });
       });
+
+    peerConnection.onicecandidate = function(event) {
+      if (event.candidate) {
+        console.log('got local candidate');
+        wsSendObject(signalingServer, {
+         candidate: event.candidate,
+         localPeerConnectionId: id,
+         targetId: remoteId
+        });
+      }
+    };
 
     return {
       id: id,
@@ -126,6 +167,10 @@
 
   function range(length, valueFunction) {
     return Array.apply(null, Array(length)).map(function () { return valueFunction(); });
+  }
+
+  function wsSendObject(ws, obj, errorCallback) {
+    ws.send(JSON.stringify(obj), errorCallback);
   }
 
 })();
