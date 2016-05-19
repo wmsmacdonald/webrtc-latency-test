5a2b8bcbc1be60b217a8d97d04224d08d2c0f8b2
William MacDonald <wmsmacdonald@gmail.com>
2016-05-18 21:22:44 -0500

    answer

--- public/javascripts/peer.js
+++ public/javascripts/peer.js
@@ -2,9 +2,9 @@
 
 (function() {
   const WS_HOST = 'ws://127.0.0.1:3131';
-  var signalingServer = new WebSocket(WS_HOST);
+  let signalingServer = new WebSocket(WS_HOST);
 
-  var signalingServerConnectPromise = new Promise(function(resolve, reject) {
+  let signalingServerConnectPromise = new Promise(function(resolve, reject) {
     signalingServer.onopen = resolve.bind(null, signalingServer);
   });
 
@@ -15,15 +15,15 @@
       }))
     });
 
-  var peerConnectionIdSeq = 0;
-  var peerConnections = range(5, function() {
+  let peerConnectionIdSeq = 0;
+  let peerConnections = range(5, function() {
     return createOfferConnection(signalingServerConnectPromise, peerConnectionIdSeq++);
   });
 
   function createOfferConnection(signalingServerConnectPromise, id) {
     console.log('creating offer connection');
-    var peerConnection = new RTCPeerConnection({'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]});
-    var offerPromise = peerConnection.createOffer();
+    let peerConnection = new RTCPeerConnection({'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]});
+    let offerPromise = peerConnection.createOffer();
 
     Promise.all([offerPromise, signalingServerConnectPromise])
       .then(function (values) {
@@ -35,9 +35,18 @@
         }));
       });
 
-    var localDescriptionPromise = offerPromise
+    offerPromise
       .then(function(description) {
         return peerConnection.setLocalDescription(description);
+      });
+
+    signalingServerConnectPromise
+      .then(function(signalingServer) {
+        peerConnection.onIceCandidate = function(candidate) {
+          signalingServer.send(JSON.stringify({
+
+          }))
+        }
       });
 
     return {
@@ -46,40 +55,35 @@
     };
   }
 
+  signalingServerConnectPromise
+    .then(function(signalingServer) {
+      signalingServer.onmessage = serverMessageController;
+    });
 
-  let messagePromise = signalingServerConnectPromise
-    .then(function onMessage() {
-      return new Promise(function(resolve, reject) {
-        signalingServer.onmessage = resolve;
+  function serverMessageController(event) {
+    let message = safelyParseJSON(event.data);
+    console.log(message);
+
+    if (message.offer && message.offer.description) {
+      // connection and answer need to be created;
+      peerConnections.push(createAnswerConnection(message.offer.description, message.remoteId, peerConnectionIdSeq++));
+    }
+
+    else if (message.answer && message.answer.description) {
+      console.log('got remote answer');
+      let peerConnection = peerConnections.find(function(peerConnection) {
+        return peerConnection.id === message.localConnectionId;
       });
-    });
 
+      peerConnection.peerConnection.setRemoteDescription(new RTCSessionDescription(message.answer.description));
+    }
 
-  messagePromise
-    .then(function wsController(event) {
-      var message = safelyParseJSON(event.data);
-      console.log(message);
-      if (message) {
-        if (message.offer && message.offer.description) {
-          // connection and answer need to be created
-          peerConnections.push(createAnswerConnection(message.offer.description, message.targetId, peerConnectionIdSeq++));
-        }
-
-        else if (message.answer && message.answer.description) {
-          let peerConnection = peerConnections.find(function(peerConnection) {
-            return peerConnection.id === message.targetId;
-          });
-
-          peerConnection.peerConnection.setRemoteDescription(message.answer.description);
-        }
-
-      }
-    });
+  }
 
   function createAnswerConnection(remoteDescription, remoteId, id) {
     console.log('creating ');
-    var peerConnection = new RTCPeerConnection({'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]});
-    var answerPromise = peerConnection.setRemoteDescription(remoteDescription)
+    let peerConnection = new RTCPeerConnection({'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]});
+    let answerPromise = peerConnection.setRemoteDescription(remoteDescription)
       .then(function() {
         return peerConnection.createAnswer();
       });
@@ -91,11 +95,13 @@
 
     Promise.all([answerPromise, signalingServerConnectPromise])
       .then(function(values) {
+        console.log('answer created');
         values[1].send(JSON.stringify({
           answer: {
             description: values[0]
           },
-          targetId: remoteId
+          targetId: remoteId,
+          localConnectionId: id
         }));
       });
 
